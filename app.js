7d310e5487df1e3ff53223c871b7957892054fc4
Igor Costa <igor@tks.global>
2026-08-09 18:56:46 -0300

    feat: estabelece conexao com banco de dados sqlite

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
 
 const PORT = 3000;
 
 app.listen(PORT, function(){
     console.log(`O express está rodando na porta ${PORT}`);
 });
+
+//db connection
+db
+  .authenticate()
+  .then(()=>{
+    console.log("Conectou ao banco com sucesso");
+  })
+  .catch(err =>{
+    console.log("Ocorreu um erro ao conectar com o banco de dados", err);
+  })
+
+// routes
 
 app.get('/', (req, res) =>{
     res.send("Está funcionando");
